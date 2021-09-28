92426c863a531113ebddd6ad461c858ffd34a4a7
anton <antopavlu@gmail.com>
2021-09-28 13:22:59 +0300

    initial commit

---
+++ src/data.js
@@ -0,0 +1,22 @@
+export const myImages = [
+  {
+    label: "First",
+    imgPath:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQG4VdYJufKr1KbT3e0WcnQ0A6b7XW3Wg7FvA&usqp=CAU",
+  },
+  {
+    label: "Second",
+    imgPath:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRor9Yz6KhQmojO47Jd7stTYDvxC9DPtX4G-8e3frUlV2WTOy3TNtywi-6qifNA__lD7A&usqp=CAU",
+  },
+  {
+    label: "Third",
+    imgPath:
+      "https://snappygoat.com/b/32fb1051945fba80d10d57a4d3cd482da9652636",
+  },
+  {
+    label: "Four",
+    imgPath:
+      "https://cdn.pixabay.com/photo/2015/12/04/16/52/background-image-1076750_640.jpg",
+  },
+];
